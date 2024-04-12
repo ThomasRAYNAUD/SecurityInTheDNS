ca1723dff55ef897f443d30efbbba776e007bf34
ThomasRAYNAUD <toto.raynaud@sfr.fr>
2024-04-12 11:54:00 +0200

    modif DoT script

--- DoT/script.py
+++ DoT/script.py
@@ -1,10 +1,20 @@
 import dns.query
-
-import dns.query
 import dns.message
 
-request = dns.message.make_query('google.com', dns.rdatatype.ANY)
+list_ip = []
+with open('../List/nameservers-all.txt', 'r') as file:
+    for line in file:
+        list_ip.append(line.strip())
 
-response = dns.query.tls(request, '8.8.8.8')
+i=0
+for ip in list_ip:
+    request = dns.message.make_query('google.com', dns.rdatatype.ANY)
+    response = dns.query.tls(request, ip)
+    print(response)
+    i+=1
+    if i == 5:
+        break
 
-print(response)
+
+# débugger l'IPv6 qui ne fonctionne pas
+# faire un whois pour récupérer qui c'est l'adresse IP
