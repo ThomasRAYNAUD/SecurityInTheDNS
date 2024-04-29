6cf6d0d19ed6eb8f7189ba683a47dc42919ad353
ThomasRAYNAUD <toto.raynaud@sfr.fr>
2024-04-29 14:54:07 +0200

    DoH

--- DoH/script.py
+++ DoH/script.py
@@ -3,43 +3,43 @@ import threading
 
 import matplotlib.pyplot as plt
 
-def run_dig_command(ip_address, dot_ips, non_dot_ips):
-    command = f"dig @{ip_address} +tls -p 853"
+def run_dig_command(ip_address, doh_ips, non_doh_ips):
+    command = f"dig @{ip_address} +https"
     try:
         result = subprocess.run(command, shell=True, capture_output=True, text=True, timeout=5) 
-        print("DoT implémenté pour l'adresse IP:", ip_address)
-        dot_ips.append(ip_address)
+        print("DoH implémenté pour l'adresse IP:", ip_address)
+        doh_ips.append(ip_address)
     except subprocess.TimeoutExpired:
         print("Timeout occurred while executing command for", ip_address)
-        print("DoT non implémenté pour l'adresse IP:", ip_address)
-        non_dot_ips.append(ip_address)
+        print("DoH non implémenté pour l'adresse IP:", ip_address)
+        non_doh_ips.append(ip_address)
     except subprocess.CalledProcessError as e:
         print(f"Error occurred while executing command for {ip_address}: {e}")
-        print("DoT non implémenté pour l'adresse IP:", ip_address)
+        print("DoH non implémenté pour l'adresse IP:", ip_address)
 
 def main():
-    dot_ips = []
-    non_dot_ips = []
+    doh_ips = []
+    non_doh_ips = []
     threads = []
 
     with open('../List/nameservers.txt', 'r') as file:
         for line in file:
             ip_address = line.strip()
             if ip_address:
-                thread = threading.Thread(target=run_dig_command, args=(ip_address, dot_ips, non_dot_ips))
+                thread = threading.Thread(target=run_dig_command, args=(ip_address, doh_ips, non_doh_ips))
                 thread.start()
                 threads.append(thread)
 
     for thread in threads:
         thread.join()
 
-    labels = ['DoT Implémenté', 'DoT Non Implémenté']
-    sizes = [len(dot_ips), len(non_dot_ips)]
+    labels = ['DoH Implémenté', 'DoH Non Implémenté']
+    sizes = [len(doh_ips), len(non_doh_ips)]
     colors = ['green', 'red']
     explode = (0.1, 0) 
     plt.pie(sizes, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%', startangle=140)
     plt.axis('equal') 
-    plt.title('Implémentation de DNS-over-TLS (DoT)')
+    plt.title('Implémentation de DNS-over-HTTPS (DoH)')
     plt.show()
 
 if __name__ == "__main__":
