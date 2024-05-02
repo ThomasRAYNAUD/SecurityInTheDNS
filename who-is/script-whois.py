f0f177ef1d261f924bc5768c287c1496a08be7ef
ThomasRAYNAUD <toto.raynaud@sfr.fr>
2024-05-02 10:50:46 +0200

    all servers v2

--- who-is/script-whois.py
+++ who-is/script-whois.py
@@ -1,17 +1,34 @@
-import whois
-import time
+import subprocess
+import re
+
+i=0
+
 def get_orgname(ip_address):
+    command = f"whois {ip_address}"
     try:
-        domain = whois.whois(ip_address)
-        domain_name = domain.domain_name if domain.domain_name else "Unknown"
-        return domain_name
-    except Exception as e:
-        #print(f"Error fetching WHOIS information for {ip_address}: {e}")
+        result = subprocess.run(command, shell=True, capture_output=True, text=True, timeout=3)
+        return result.stdout.strip()
+    except subprocess.TimeoutExpired:
+        print(f"Timeout expired while fetching WHOIS information for {ip_address}")
         return "Unknown"
+    except Exception as e:
+        print(f"Error fetching WHOIS information for {ip_address}: {e}")
+        return "Unknown"
+
+def get_netname(whois_output):
+    netname_match = re.search(r'OrgName:\s+([^\n\r]+)', whois_output, re.IGNORECASE)
+    if netname_match:
+        return netname_match.group(1)
+    else:
+        return "NetName non trouvé"
 
 with open('../IP_address/public_address.txt', 'r') as file:
     for ip in file:
-        if ip == '\n':
-            continue
+        ip = ip.strip()
         orgname = get_orgname(ip)
-        print(f"OrgName: {orgname}")
+        netname = get_netname(orgname)
+        if netname != "NetName non trouvé":
+            i+=1
+            print(f"IP: {ip} - NetName: {netname}")
+            
+print(f"Nombre d'IP avec NetName trouvé: {i}")
--- who-is/tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-def get_orgname(ip_address):
-    command = f"nslookup {ip_address} | grep 'name =' | awk '{{print $4}}'"
-    try:
-        result = subprocess.check_output(command, shell=True).decode().strip()
-        domain_name = result if result else "Unknown"
-        return domain_name
-    except Exception as e:
-        print(f"Error fetching WHOIS information for {ip_address}: {e}")
-        return "Unknown"
