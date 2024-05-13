22d05c203038994242a972b979c20ee536a9ae40
Nguyen Nelly <nnguyen@2D-LINUX-045.insa-lyon.fr>
2024-05-13 18:48:08 +0200

    DNSSEC json

--- remplirJSON/nellly/script2.py
+++ remplirJSON/nellly/script2.py
@@ -4,6 +4,33 @@ import json
 if __name__ == "__main__":
     with open('donnees.json', 'r') as f:
         contenu = json.load(f)
+        for cle in contenu:
+            for j in contenu[cle]:
+                print(j['ip'])
+                with open("DNSSEC_RESOLVERS.txt", "r") as file:
+                    content = file.read()
+                    if j['ip'] in content:
+                        j["DNSSEC"] = "Yes"
+                    else:
+                        j["DNSSEC"] = "No"
+                try:
+                    with open('./DNSSEC.json', 'r') as f:
+                        contenu_json = json.load(f)
+                except FileNotFoundError:
+                    contenu_json = {}
+                except json.decoder.JSONDecodeError as e:
+                    print(f"Error decoding JSON: {e}")
+                    contenu_json = {}
+                contenu_json[cle] = j
+
+                with open('./DNSSEC.json', 'w') as f:
+                    json.dump(contenu_json, f, indent=4)
+
+
+"""
+if __name__ == "__main__":
+    with open('donnees.json', 'r') as f:
+        contenu = json.load(f)
         for i in contenu:
             for j in contenu[i]:
                 print(j['ip'])
@@ -39,3 +66,4 @@ if __name__ == "__main__":
 
                 with open('./DNSSEC.json', 'w') as f:
                     json.dump(contenu, f, indent=4)
+"""
