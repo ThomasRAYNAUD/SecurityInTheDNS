533493adff48ce7671265f225f4af81f1ce87a2d
Nguyen Nelly <nnguyen@2D-LINUX-079.insa-lyon.fr>
2024-05-14 17:07:48 +0200

    /

--- DNSSEC/script.py
+++ DNSSEC/script.py
@@ -1,42 +1,42 @@
 import json
 import matplotlib.pyplot as plt
 
-nb_Cloudflare = 0
-nb_Google_server = 0
-nb_atnt_server = 0
-nb_amazon_server = 0
-nb_level3_server = 0
-nb_cisco_server = 0
-others = 0
-
 
 if __name__ == "__main__":
+    nb_Cloudflare = 0
+    nb_high = 0
+    nb_nucdn = 0
+    nb_level3_server = 0
+    nb_nextdns_server = 0
     nb_DNSSEC = 0
+    nb_akamai = 0
+    nb_comcast = 0
+
     with open("../remplirJSON/nellly/DNSSEC.json", 'r') as f:
         contenu = json.load(f)
         for i in contenu:
             for j in contenu[i]:
                 if j['DNSSEC'] == "Yes":
                     nb_DNSSEC += 1
-                    if j['orgName'] == "Cloudflare, Inc.":
+                    if j['orgName'] == "CLOUDFLARENET":
                         nb_Cloudflare += 1
-                    elif j['orgName'] == "Google LLC":
-                        nb_Google_server += 1
-                    elif j['orgName'] == "AT&T Corp.":
-                        nb_atnt_server += 1
-                    elif j['orgName'] == "Amazon Technologies Inc.":
-                        nb_amazon_server += 1
-                    elif j['orgName'] == "Level 3 Parent, LLC":
+                    elif j['orgName'] == "Hi-Tec Enterprise":
+                        nb_high += 1
+                    elif j['orgName'] == "NUCDN":
+                        nb_nucdn+= 1
+                    elif j['orgName'].startswith("Akamai"):
+                        nb_akamai+= 1
+                    elif j['orgName'] == "UUNET":
                         nb_level3_server += 1
-                    elif j['orgName'] == "Cisco OpenDNS, LLC":
-                        nb_cisco_server += 1
-                    else :
-                        others += 1
+                    elif j['orgName'] == "nextdns, Inc.":
+                        nb_nextdns_server += 1
+                    elif j['orgName'].startswith("COMCAST"):
+                        nb_comcast += 1 
     
-    labels = ["Cloudflare, Inc.", "Google LLC", "AT&T Corp.", "Amazon Technologies Inc.", "Level 3 Parent, LLC", "Cisco OpenDNS, LLC"]
-    sizes = [nb_Cloudflare, nb_Google_server, nb_atnt_server, nb_amazon_server, nb_level3_server, nb_cisco_server]
-    pourcentages = [nb_Cloudflare/nb_DNSSEC*100, nb_Google_server/nb_DNSSEC*100, nb_atnt_server/nb_DNSSEC*100, nb_amazon_server/nb_DNSSEC*100, nb_level3_server/nb_DNSSEC*100, nb_cisco_server/nb_DNSSEC*100]
-    colors = ['cornflowerblue', 'cornflowerblue','cornflowerblue','cornflowerblue','cornflowerblue','cornflowerblue']
+    labels = ["Cloudflare, Inc.", "nextdns","nucdn","Akamai", "COMCAST","Hi-Tec Enterprise","UUNET"]
+    sizes = [nb_Cloudflare, nb_nextdns_server, nb_nucdn, nb_akamai, nb_comcast, nb_high, nb_level3_server]
+    pourcentages = [nb_Cloudflare/nb_DNSSEC*100, nb_nextdns_server/nb_DNSSEC*100, nb_nucdn/nb_DNSSEC*100,nb_akamai/nb_DNSSEC*100, nb_comcast/nb_DNSSEC*100,nb_high/nb_DNSSEC*100,nb_level3_server/nb_DNSSEC*100]
+    colors = ['cornflowerblue', 'cornflowerblue','cornflowerblue','cornflowerblue','cornflowerblue','cornflowerblue','cornflowerblue','cornflowerblue','cornflowerblue']
 
     rects = plt.bar(labels, sizes, color=colors)
     plt.ylabel('Nombre de résolveurs')
--- DNSSEC/test.py
+++ DNSSEC/test.py
@@ -1,9 +1,15 @@
 import json
 import subprocess
 
-if __name__ =="__main__": 
-    with open("./DNSSEC.json", 'r') as f:
+if __name__ =="__main__":
+    dico = {}
+    with open("../remplirJSON/nellly/DNSSEC.json", 'r') as f:
         contenu = json.load(f)
         for i in contenu:
-            contenu[i] = [contenu[i]]
-            
+            for j in contenu[i]:
+                if j['DNSSEC'] == "Yes":
+                    if j['orgName'] not in dico :
+                        dico[j['orgName']] = 1
+                    elif j['orgName'] in dico :
+                        dico[j['orgName']] += 1
+        print(dico)
